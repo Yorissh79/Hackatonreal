4ff603d4b791fb647c0c07ce4386ed6169bdb4e5
NigarHasanzada <nigarnh-ap216@code.edu.az>
2025-07-01 11:08:16 +0400

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import "./Header.css"
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="navbar-area">
+            <div className="navbar-component">
+                <div className="loogo">
+                    <h1>RoomEase</h1>
+                </div>
+                <hr className='hr'/>
+                <div className="navlist">
+                    <ul className='navlist-items'>
+                        <li className='navlist-item'>DESTINATION</li>
+                        <li className='navlist-item'>EXPIARENCES</li>
+                        <li className='navlist-item'>VILLAS</li>
+                        <li className='navlist-item'>PRIVATE HOMES</li>
+                        <li className='navlist-item'>STORIES</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
