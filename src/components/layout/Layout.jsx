54b08d1a67585d0001649233d5f247e9d4ea0d84
Yorissh79 <yusifbsh-azmp203@code.edu.az>
2025-07-01 09:21:16 +0400

    start

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "../header/Header.jsx";
+import {Outlet} from "react-router";
+import Footer from "../footer/Footer.jsx";
+
+const Layout = () => {
+    return (
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
+export default Layout
