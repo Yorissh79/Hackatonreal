c83eaabc9df6bc3a342b5722de95d7d681867892
Yorissh79 <yusifbsh-azmp203@code.edu.az>
2025-07-01 10:41:36 +0400

    admin

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout/Layout.jsx";
 import Home from "../pages/home/Home.jsx";
 import RegisterForm from "../pages/register/Register.jsx";
 import LoginForm from "../pages/login/Login.jsx";
+import Admin from "../pages/admin/Admin.jsx";
+import adminLayout from "../components/adminlayout/AdminLayout.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -23,4 +25,14 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/admin",
+    Component: adminLayout,
+    children: [
+      {
+        path: "/",
+        Component: Admin,
+      }
+    ]
+  }
 ]);
